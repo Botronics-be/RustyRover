2524013ffc913664a3e7d7bf891f84449b54d182
Nicodaube <nicolas.daube@botronics.be>
2026-01-22 09:42:58 +0100

    Bug with run ble server

--- rusty_rover_ws/src/rusty_ble_server/src/main.rs
+++ rusty_rover_ws/src/rusty_ble_server/src/main.rs
@@ -1,15 +1,95 @@
 use rclrs::*;
 use std::{thread, time::Duration};
 
+use bluer::{
+    adv::Advertisement,
+    gatt::local::{
+        Application, Characteristic, CharacteristicRead,
+        CharacteristicWrite, Service,
+    }, Uuid,
+};
+use serde::Deserialize;
+
+// Service and characteristic UUID used to identify the ble server
+const SERVICE_UUID: Uuid = Uuid::from_u128(0x12345678_1234_5678_1234_567812345678);
+const CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0x87654321_4321_6789_4321_678943216789);
+
+// Default BLE message structure
+#[derive(Deserialize, Debug)]
+struct Message {
+    #[serde(rename = "type")]
+    msg_type: String,
+    data: String,
+}
+
 pub struct BleServerNode {
+    node: Arc<Node>,
 }
 
 impl BleServerNode {
-    fn new(executor: &Executor) -> Result<Self, RclrsError> {
+    fn new(executor: &Executor) -> Result<Self, RclrsError> {        
         let node = executor.create_node("bluetooth_server_node")?;
+        log_info!(node.logger(), "Bluetooth_server_node Startup");
 
-        Ok(Self { })
+        let node_clone = node.clone()
+
+        // Create a separate thread to hold the async bluer Server logic
+        thread::spawn(move || {
+            let rt = tokio::runtime::Runtime::new().unwrap();
+            
+            rt.block_on(async {
+                if let Err(e) = Self::run_bluetooth_server(node_clone).await {
+                    eprintln!("Bluetooth error: {}", e);
+                }
+            });
+        });
+
+        Ok(Self { node })
     }
+
+    asynvc fn run_bluetooth_server(node: Arc<Node>) -> bluer::Result<()> {
+        log_info!(node.logger(), "Initializing Bluetooth Session...");
+
+        // Creating session and powering the adapter on
+        let session = bluer::Session::new().await?;
+        let adapter = session.default_adapter().await?;
+        adapter.set_powered(true).await?;
+
+        let le_advertisement = Advertisement {
+            service_uuids: vec![SERVICE_UUID].into_iter().collect(),
+            discoverable: Some(true),
+            local_name: Some("RustyRover".to_string()),
+            ..Default::default()
+        };
+        
+        let _adv_handle = adapter.advertise(le_advertisement).await?;
+
+        log_info!(node.logger(), "BLE Server available !");
+
+        std::future::pending::<()>().await;
+
+        Ok(())
+    }
+
+    // fn advertise_server() {
+
+    //     // start the session and adapter
+    //     let session = bluer::Session::new().await?;
+    //     let adapter = session.default_adapter().await?;
+    //     adapter.set_powered(true).await?;
+
+    //     let ble_server_name = "RustyRover";
+
+    //     let le_advertisement = Advertisement {
+    //         service_uuids: vec![SERVICE_UUID].into_iter().collect(),
+    //         discoverable: Some(true),
+    //         local_name: Some("ble_server_name".to_string()),
+    //         ..Default::default()
+    //     };
+    //     let adv_handle = adapter.advertise(le_advertisement).await?;
+
+    //     log_info!(node.logger(), "Bluetooth server available");
+    // }
 
 }
 
