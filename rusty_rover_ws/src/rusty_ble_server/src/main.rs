95c4cd8778d8d9fdc9ebbc3a3ab584128e69c4f0
Nicodaube <nicolas.daube@botronics.be>
2026-01-22 09:08:53 +0100

    Basic node ble

--- rusty_rover_ws/src/rusty_ble_server/src/main.rs
+++ rusty_rover_ws/src/rusty_ble_server/src/main.rs
@@ -1,61 +1,21 @@
 use rclrs::*;
 use std::{thread, time::Duration};
-use std_msgs::msg::String as StringMsg;
 
-pub struct Middleman {
-    worker: Worker<Option<String>>,
-    publisher: Publisher<StringMsg>,
+pub struct BleServerNode {
 }
 
-impl Middleman {
+impl BleServerNode {
     fn new(executor: &Executor) -> Result<Self, RclrsError> {
-        let node = executor.create_node("simple_subscription")?;
-        let worker = node.create_worker(None);
-        let publisher = node.create_publisher("republishing_hello")?;
-        worker.create_subscription::<StringMsg, _>(
-            "publish_hello",
-            move |data: &mut Option<String>, msg: StringMsg| {
-                *data = Some(msg.data);
-            },
-        )?;
+        let node = executor.create_node("bluetooth_server_node")?;
 
-        Ok(Self { worker, publisher })
+        Ok(Self { })
     }
 
-    fn republish(&self) -> Result<(), RclrsError> {
-        let publisher = self.publisher.clone();
-        drop(
-            self.worker
-                .run(move |data: &mut Option<String>| -> Result<(), RclrsError> {
-                    data.clone()
-                        .map(|message| {
-                            publisher.publish(StringMsg {
-                                data: message.clone(),
-                            })
-                        })
-                        .ok_or(RclrsError::RclError {
-                            code: RclReturnCode::SubscriptionTakeFailed,
-                            msg: None,
-                        })??;
-                    Ok(())
-                }),
-        );
-        Ok(())
-    }
 }
 
 fn main() -> Result<(), RclrsError> {
     let mut executor = Context::default_from_env()?.create_basic_executor();
-    let node = Middleman::new(&executor)?;
-
-    thread::spawn(move || {
-        loop {
-            thread::sleep(Duration::from_secs(1));
-            if let Err(e) = node.republish() {
-                eprintln!("Republish error: {}", e);
-            }
-        }
-    });
+    let node = BleServerNode::new(&executor)?;
 
     executor.spin(SpinOptions::default()).first_error()
 }
