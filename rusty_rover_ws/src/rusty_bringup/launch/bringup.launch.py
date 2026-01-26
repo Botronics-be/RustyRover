966756a2e901118aa57355923eccc39ab585fa6a
davidmoli-botronics <david.moli@botronics.be>
2026-01-26 21:40:51 +0100

    bringup and launch refactor

--- rusty_rover_ws/src/rusty_bringup/launch/bringup.launch.py
+++ rusty_rover_ws/src/rusty_bringup/launch/bringup.launch.py
@@ -1,83 +1,54 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetEnvironmentVariable
-from launch.substitutions import LaunchConfiguration, Command
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, TimerAction
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
-
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+
     sim_mode = LaunchConfiguration('sim_mode')
     
     sim_mode_arg = DeclareLaunchArgument(
         'sim_mode',
         default_value='true',
-        description='Use simulation (Gazebo) if true'
+        choices=['true', 'false'],
+        description='Set to "true" to launch Gazebo. Set to "false" for physical hardware.'
     )
 
-    gz_pkg_share =  get_package_share_directory('rusty_gz')
-    description_pkg_share = get_package_share_directory('rusty_description')
-    
-    robot_description_content = Command([
-        'xacro ', os.path.join(description_pkg_share, 'urdf', 'rusty.urdf.xacro'),
-        ' sim_mode:=', sim_mode
-    ])
-    robot_description = {
-        'robot_description': ParameterValue(robot_description_content, value_type=str)
-    }
+    pkg_description = FindPackageShare('rusty_description')
+    pkg_gz          = FindPackageShare('rusty_gz')
+    pkg_control     = FindPackageShare('rusty_control')
     
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                os.path.join(
-                    get_package_share_directory('rusty_description'),
-                    'launch',
-                    'rsp.launch.py',
-                )
-            ]
-        ),
-        launch_arguments={
-            'sim_mode': sim_mode,
-        }.items(),
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([pkg_description, 'launch', 'rsp.launch.py'])
+        ]),
+        launch_arguments={'sim_mode': sim_mode}.items(),
     )
 
     gz_bringup = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                os.path.join(
-                    get_package_share_directory('rusty_gz'),
-                    'launch',
-                    'gz_bringup.launch.py',
-                )
-            ]
-        ),
-        launch_arguments={
-            'sim_mode': sim_mode,
-        }.items(),
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([pkg_gz, 'launch', 'gz_bringup.launch.py'])
+        ]),
+        launch_arguments={'sim_mode': sim_mode}.items(),
+        condition=IfCondition(sim_mode)  
     )
 
     control = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                os.path.join(
-                    get_package_share_directory('rusty_control'),
-                    'launch',
-                    'control.launch.py',
-                )
-            ]
-        ),
-        launch_arguments={
-            'sim_mode': sim_mode,
-        }.items(),
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([pkg_control, 'launch', 'control.launch.py'])
+        ]),
+        launch_arguments={'sim_mode': sim_mode}.items(),
     )
 
     return LaunchDescription([
         sim_mode_arg,
         rsp,
         gz_bringup,
-        control,
+        # Small delay to ensure we have our robot set before our controllers
+        TimerAction(
+            period=2.0,
+            actions=[control]
+        )
     ])
