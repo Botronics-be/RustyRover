f1044f60ff826e525ac4bfe5e796ad9c620b1160
Nicodaube <nicolas.daube@botronics.be>
2026-01-27 17:10:59 +0100

    Created command dispatcher

---
+++ rusty_rover_ws/src/rusty_command_dispatcher/src/command_dispatcher.rs
@@ -0,0 +1,10 @@
+use geometry_msgs::msg::TwistStamped as TwistStamped;
+use rusty_msgs::msg::Teleop as TeleopMsg;
+use std_msgs::msg::Header;
+use geometry_msgs::msg::Twist;
+use geometry_msgs::msg::Vector3;
+use builtin_interfaces::msg::Time;
+
+fn main() {
+    println!("Hello, world!");
+}
